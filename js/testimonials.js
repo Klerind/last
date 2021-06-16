7fb7f231b9a759d7dadfb53c0b41350c9ff6bab6
Klerind <klerindtervoli@gmail.com>
2021-06-17 00:27:18 +0200

    added json api

--- js/testimonials.js
+++ js/testimonials.js
@@ -1,33 +1,109 @@
-const xhttp = new XMLHttpRequest();
-xhttp.onload = function(){
-  const person_info = JSON.parse(this.responseText);
-  const testimonials = document.querySelector("#testimonials div");
-  for (var i = 0; i < person_info.length; i++) {
-    let innerDivText = "";
-      innerDivText += "<div><span>0"+(1+i)+"</span>"
-      innerDivText += "<h2>"+person_info[i].name+" "+person_info[i].surename+"</h2>";
-      innerDivText += "<h3>"+person_info[i].email+"</h3>";
-      innerDivText += "<h4>"+person_info[i].address+"</h4>";
-      innerDivText += "</div>"
-      testimonials.innerHTML += innerDivText;
+//global variables
+const newClientButton = document.getElementById("new-client");
+const tableBody = document.getElementById("tbody");
+const addClientForm = document.getElementById("add-client-form");
+let tableBodyContent = "";
+//gets data with ajax from json/outputs.json file
+function getData(data) {
+  //converts data from json format to javascript object and saves it to a constsnt variable
+    const tableData = JSON.parse(data.responseText);
+    //calls printDataToTable function and passes tableData variable into it
+    printDataToTable(tableData);
+}
+//calls getAjaxRequest function from js/_ajaxFunctions.js file
+getAjaxRequest("json/inputs.json",getData);
+//prints data from json/inputs.json to table body
+function printDataToTable(tableData) {
+  for (var i = 0; i < tableData.clients.length; i++) {
+    tableBodyContent += "<tr><td>"+tableData.clients[i].name+"</td><td>"+tableData.clients[i].email+"</td><td>"+tableData.clients[i].phone+"</td><td>";
+    for (var ii = 0; ii < tableData.clients[i].providers.length; ii++) {
+      if (tableData.clients[i].providers[ii].id == 1 ) {
+      tableBodyContent +=  tableData.providers[0].name +", ";
+    }else if (tableData.clients[i].providers[ii].id == 2) {
+      tableBodyContent +=  tableData.providers[1].name +", ";
+    }else if (tableData.clients[i].providers[ii].id == 3) {
+      tableBodyContent +=  tableData.providers[2].name +", ";
+    }else if (tableData.clients[i].providers[ii].id == 4) {
+      tableBodyContent +=  tableData.providers[3].name +", ";
+    }else if (tableData.clients[i].providers[ii].id == 5) {
+      tableBodyContent +=  tableData.providers[4].name +", ";
+     }
+    }
+    tableBodyContent += "</td><td onclick='editRow(this)'>edit</td><td onclick='deleteRow(this)'>delete</td></tr>";
+    tableBody.innerHTML = tableBodyContent;
   }
 }
-xhttp.open("GET","json/table.json",true);
-xhttp.send();
-let mydata = [
-  "greta",
-  "tervoli",
-  "email\n",
-  "address\n",
-   {"fullname":()=>{return mydata[0]+" "+mydata[1];}}
- ];
-//console.log(mydata[4].fullname());
-
-const objectOne = {
-  "name":"Klerind",
-  "email":"myemail",
-  "address":"myhomeaddress"
+//displays new client form when new client button is clicked
+newClientButton.addEventListener("click", function() {
+  addClientForm.style.display = "block";
+});
+//deletes table row when delete button is clicked
+function deleteRow(event) {
+  //gets data from json/inputs.json file
+    function getData(data) {
+        const deleteClient = JSON.parse(data.responseText);
+        deleteClient.clients.splice(event.parentNode.rowIndex - 1,1);
+        const saveClient = JSON.stringify(deleteClient);
+        //calls saveClientData function and passes saveClient variable into it
+        saveClientData(saveClient);
+    }
+//calls getAjaxRequest function from js/_ajaxFunctions.js file and passes a string url and a function name into it
+getAjaxRequest("json/inputs.json",getData);
+//sends updete data from the editted row to php/_delete.php file
+function saveClientData(saveClient) {
+       sendAjaxRequest("php/_saveClient.php",saveClient);
+    }
+    //reloads page after 3 second
+    setTimeout(()=>{
+      window.location.reload();
+    },300);
 }
-let objects = [];
-objects[0] = objectOne;
-//console.log(objects[0]);
+//edits table row when edit button is clicked
+function editRow(event) {
+  let rowData = [];
+    if (event.parentNode.firstChild.contentEditable == "inherit") {
+    for (var i = 0; i < 4; i++) {
+      //setAttribute contenteditable from col 1 up to 4
+      event.parentNode.children[i].setAttribute("contenteditable","true");
+      //setAttribute style cursor pointer from col 1 up to col 4
+      event.parentNode.children[i].setAttribute("style","cursor:pointer;");
+       }
+      //sets edit button to save button
+      event.parentNode.children[4].innerText = "save";
+      event.parentNode.children[4].style.color = "tomato";
+    }else{
+     for (var i = 0; i < 4; i++) {
+      //setAttribute contenteditable from col 1 up to 4
+      event.parentNode.children[i].setAttribute("contenteditable","inherit");
+      //setAttribute style cursor pointer from col 1 up to col 4
+      event.parentNode.children[i].removeAttribute("style");
+      rowData[i] = event.parentNode.children[i].textContent;
+    }
+    //gets data from json/inputs.json file
+    function getData(data) {
+      //converts data from json format to javascript object
+       let parsedData = JSON.parse(data.responseText);
+       for (var i = 0; i < rowData.length; i++) {
+         //updates rows name
+         parsedData.clients[event.parentElement.rowIndex - 1].name = rowData[0];
+         //updates rows email
+         parsedData.clients[event.parentElement.rowIndex - 1].email = rowData[1];
+         //updates rows phone
+         parsedData.clients[event.parentElement.rowIndex - 1].phone = rowData[2];
+       }
+       //converts javascript object to json format
+       const jsonData = JSON.stringify(parsedData);
+       //sends data to php/_saveClient.php file
+       sendAjaxRequest("php/_saveClient.php",jsonData);
+       //reloads page after 3 second
+       setTimeout(()=>{
+         window.location.reload();
+       },300);
+    }
+    //gets data from json/inputs.json file and passes it to getData function
+    getAjaxRequest("json/inputs.json",getData);
+    //sets save button to edit button
+    event.parentNode.children[4].innerText = "edit";
+    event.parentNode.children[4].style.color = "blue";
+  }
+}
--- json.php
+++ json.php
@@ -1,5 +1,51 @@
 <?php include_once "header.php"; ?>
 <section id="testimonials">
+  <button id="new-client" class="btn btn-primary" type="button" name="button">new client</button>
+     <table class="table">
+  <thead>
+    <th>name</th>
+    <th>email</th>
+    <th>phone</th>
+    <th>provides</th>
+  </thead>
+  <tbody id="tbody"></tbody>
+</table>
+<div id="add-client-form">
+  <form action="php/_addClient.php" method="get">
+  new client
+  <div class="mb-3 row">
+    <label for="inputPassword" class="col-sm-2 col-form-label">name:</label>
+    <div class="col-sm-10">
+      <input type="text" name="name" class="form-control" id="inputPassword" required>
+    </div>
+  </div>
+  <div class="mb-3 row">
+    <label for="inputPassword" class="col-sm-2 col-form-label">email:</label>
+    <div class="col-sm-10">
+      <input type="email" name="email" class="form-control" id="inputPassword" required>
+    </div>
+  </div>
+  <div class="mb-3 row">
+    <label for="inputPassword" class="col-sm-2 col-form-label">phone:</label>
+    <div class="col-sm-10">
+      <input type="number" name="phone" class="form-control" id="inputPassword" required>
+    </div>
+  </div>
+  <div class="mb-3 row">
+    <label for="inputPassword" class="col-sm-2 col-form-label">providers:</label>
+    <div class="col-sm-10">
+      <select name="providers[]" class="selectpicker" multiple="true" required>
+       <option value="1">Provider1</option>
+       <option value="2">Provider2</option>
+       <option value="3">Provider3</option>
+       <option value="4">Provider4</option>
+       <option value="5">Provider5</option>
+     </select>
+    </div>
+  </div>
+  <input class="btn btn-success" type="submit" name="submit" value="add-client">
+  </form>
+</div>
 </section>
 <script defer="true" src="js/testimonials.js"></script>
 <?php include_once "footer.php" ?>
