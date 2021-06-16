dc475432e664ce8615c5cc960cf8c8d9dfa418bd
Klerind <klerindtervoli@gmail.com>
2021-06-16 16:38:46 +0200

    addetd weather api

--- testimonials.php
+++
@@ -1,7 +0,0 @@
-<?php include_once "header.php"; ?>
-<section id="testimonials">
-  <div>
-  </div>
-</section>
-<script defer="true" src="js/testimonials.js"></script>
-<?php include_once "footer.php" ?>
