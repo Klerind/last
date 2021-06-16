a4d42b891d8826bea2a6015577ffe141b783cd4e
Klerind <klerindtervoli@gmail.com>
2021-06-17 00:10:35 +0200

    added bootstrap to project

--- head.php
+++ head.php
@@ -4,6 +4,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>last</title>
   <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
   <script async="true" src="js/main.js"></script>
   <script defer src="js/assets/_ajaxFunctions.js"></script>
 </head>
